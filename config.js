ad3762024ac95dee18e853037a31d9b2ce18e628
Arnoud de Boer <a.deboer@geonovum.nl>
2021-07-26 07:56:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var respecConfig = {
-  specStatus: "GN-WV",
+  specStatus: "GN-VV",
   specType: "IM",
   //canonicalURI: "WV",
-//  publishDate: "2021-06-28",
-  maxTocLevel: 3,
+//  publishDate: "2021-06-23",
+  maxTocLevel: 4,
   editors: [
     {
       name: "TNO Geologische Dienst Nederland",
@@ -17,8 +17,8 @@ var respecConfig = {
   shortName: "guf",
   pubDomain: "bro",
   github: "https://github.com/BROprogramma/GUF",
-  previousPublishDate: "2021-06-28",
- previousStatus: "GN-VV",
+//  previousPublishDate: "2021-06-28",
+// previousStatus: "GN-VV",
   //localBiblio: {
   //  "ISO19136": {
   //    "href": "https://www.iso.org/standard/32554.html",
